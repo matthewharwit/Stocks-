9a1be1d7c3835ea8a9f04f060246427da052f4ec
matthewharwit <56949658+matthewharwit@users.noreply.github.com>
2021-08-23 17:05:20 -0600

    Add files via upload

---
+++ running stocks/Schwab_Gap_Downs.py
@@ -0,0 +1,982 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#Gap downs Schwab
+
+import pyautogui as pg
+
+
+import time
+
+
+# In[60]:
+
+
+#pg.position()
+
+
+# In[62]:
+
+
+
+
+#takes 1:59 min/sec to run. 
+
+#click on Schwab
+
+pg.moveTo(1259,1067)
+time.sleep(1)
+pg.click(1259,1067)
+
+#will already be on gap down
+
+pg.click(307,208)
+
+time.sleep(1)
+
+pg.click(363,209)
+
+time.sleep(2.5)
+
+pg.click(399,209)
+
+time.sleep(2.5)
+
+pg.click(448,212)
+
+time.sleep(2.5)
+
+pg.click(492,212)
+
+time.sleep(2.5)
+
+pg.click(537,212)
+
+time.sleep(2.5)
+
+pg.click(585,212)
+
+time.sleep(2.5)
+
+pg.click(617,212)
+
+time.sleep(2.5)
+
+pg.click(660,212)
+
+time.sleep(2.5)
+
+pg.click(703,206)
+
+time.sleep(2.5)
+
+pg.click(755,206)
+
+time.sleep(2.5)
+
+pg.click(810,206)
+
+time.sleep(2.5)
+
+pg.click(865,206)
+
+time.sleep(2.5)
+
+pg.click(920,206)
+
+time.sleep(2.5)
+
+pg.click(970,206)
+
+time.sleep(2.5)
+
+pg.click(1015,206)
+
+time.sleep(2.5)
+
+pg.click(1070,206)
+
+time.sleep(2.5)
+
+pg.click(1125,206)
+
+time.sleep(2.5)
+
+pg.click(1185,206)
+
+time.sleep(2.5)
+
+pg.click(1230,206)
+
+time.sleep(2.5)
+
+pg.click(307,208)
+
+time.sleep(2.5)
+
+
+#second part: directly in to gap 1% down folder
+
+#first tab (1-1.3 bil)
+pg.click(307,208)
+
+#click on results
+pg.click(399,243)
+#click on actions
+pg.click(1872,236)
+#click on export results
+
+time.sleep(0.7)
+
+pg.click(1781,579)
+time.sleep(0.5)
+
+#click on downloads 
+pg.click(117,274)
+time.sleep(0.5)
+
+#click gap 1% down folder 
+pg.moveTo(375,251)
+time.sleep(0.5)
+
+pg.click(375,251)
+pg.click(375,251)
+time.sleep(0.1)
+
+#click save as type
+pg.click(680,500)
+time.sleep(0.1)
+#click csv
+pg.click(518,560)
+time.sleep(0.1)
+
+#click save
+pg.click(747,570)
+
+#if no tickers in list, can't export, so click on ok on the error message, then go to next tab
+
+#click ok on error message
+pg.click(1296,575)
+
+
+
+#second tab (1.3-1.6)
+pg.click(363,209)
+
+#click on results
+pg.click(399,243)
+#click on actions
+pg.click(1872,236)
+#click on export results
+
+time.sleep(0.7)
+
+pg.click(1781,579)
+time.sleep(0.1)
+
+#click on downloads 
+pg.click(117,274)
+time.sleep(0.1)
+
+#click gap 1% down folder 
+pg.moveTo(418,222)
+time.sleep(0.1)
+
+pg.click(418,222)
+pg.click(418,222)
+time.sleep(0.1)
+
+#click save as type
+pg.click(680,500)
+time.sleep(0.1)
+#click csv
+pg.click(518,560)
+time.sleep(0.1)
+
+#click save
+pg.click(747,570)
+
+#if no tickers in list, can't export, so click on ok on the error message, then go to next tab
+
+#click ok on error message
+pg.click(1296,575)
+
+
+
+#third tab (1.6-1.8)
+pg.click(399,209)
+
+#click on results
+pg.click(399,243)
+#click on actions
+pg.click(1872,236)
+#click on export results
+
+time.sleep(0.7)
+
+pg.click(1781,579)
+time.sleep(0.1)
+
+#click on downloads 
+pg.click(117,274)
+time.sleep(0.1)
+
+#click gap 1% down folder 
+pg.moveTo(418,222)
+time.sleep(0.1)
+
+pg.click(418,222)
+pg.click(418,222)
+time.sleep(0.1)
+
+#click save as type
+pg.click(680,500)
+time.sleep(0.1)
+#click csv
+pg.click(518,560)
+time.sleep(0.1)
+
+#click save
+pg.click(747,570)
+
+#if no tickers in list, can't export, so click on ok on the error message, then go to next tab
+
+#click ok on error message
+pg.click(1296,575)
+
+
+
+#fourth tab (1.8-2.1)
+pg.click(448,212)
+
+#click on results
+pg.click(399,243)
+#click on actions
+pg.click(1872,236)
+#click on export results
+
+time.sleep(0.7)
+
+pg.click(1781,579)
+time.sleep(0.1)
+
+#click on downloads 
+pg.click(117,274)
+time.sleep(0.1)
+
+#click gap 1% down folder 
+pg.moveTo(418,222)
+time.sleep(0.1)
+
+pg.click(418,222)
+pg.click(418,222)
+time.sleep(0.1)
+
+#click save as type
+pg.click(680,500)
+time.sleep(0.1)
+#click csv
+pg.click(518,560)
+time.sleep(0.1)
+
+#click save
+pg.click(747,570)
+
+#if no tickers in list, can't export, so click on ok on the error message, then go to next tab
+
+#click ok on error message
+pg.click(1296,575)
+
+
+
+#fifth tab (2.1-2.5)
+pg.click(492,212)
+
+#click on results
+pg.click(399,243)
+#click on actions
+pg.click(1872,236)
+#click on export results
+
+time.sleep(0.7)
+
+pg.click(1781,579)
+time.sleep(0.1)
+
+#click on downloads 
+pg.click(117,274)
+time.sleep(0.1)
+
+#click gap 1% down folder 
+pg.moveTo(418,222)
+time.sleep(0.1)
+
+pg.click(418,222)
+pg.click(418,222)
+time.sleep(0.1)
+
+#click save as type
+pg.click(680,500)
+time.sleep(0.1)
+#click csv
+pg.click(518,560)
+time.sleep(0.1)
+
+#click save
+pg.click(747,570)
+
+#if no tickers in list, can't export, so click on ok on the error message, then go to next tab
+
+#click ok on error message
+pg.click(1296,575)
+
+#sixth tab (2.5-2.8)
+pg.click(527,212)
+
+#click on results
+pg.click(399,243)
+#click on actions
+pg.click(1872,236)
+#click on export results
+
+time.sleep(0.7)
+
+pg.click(1781,579)
+time.sleep(0.1)
+
+#click on downloads 
+pg.click(117,274)
+time.sleep(0.1)
+
+#click gap 1% down folder 
+pg.moveTo(418,222)
+time.sleep(0.1)
+
+pg.click(418,222)
+pg.click(418,222)
+time.sleep(0.1)
+
+#click save as type
+pg.click(680,500)
+time.sleep(0.1)
+#click csv
+pg.click(518,560)
+time.sleep(0.1)
+
+#click save
+pg.click(747,570)
+
+#if no tickers in list, can't export, so click on ok on the error message, then go to next tab
+
+#click ok on error message
+pg.click(1296,575)
+
+
+
+#seventh tab (2.8-3.3)
+pg.click(585,212)
+
+#click on results
+pg.click(399,243)
+#click on actions
+pg.click(1872,236)
+#click on export results
+
+time.sleep(0.7)
+
+pg.click(1781,579)
+time.sleep(0.1)
+
+#click on downloads 
+pg.click(117,274)
+time.sleep(0.1)
+
+#click gap 1% down folder 
+pg.moveTo(418,222)
+time.sleep(0.1)
+
+pg.click(418,222)
+pg.click(418,222)
+time.sleep(0.1)
+
+#click save as type
+pg.click(680,500)
+time.sleep(0.1)
+#click csv
+pg.click(518,560)
+time.sleep(0.1)
+
+#click save
+pg.click(747,570)
+
+#if no tickers in list, can't export, so click on ok on the error message, then go to next tab
+
+#click ok on error message
+pg.click(1296,575)
+
+
+
+#eigth tab (3.3-4)
+pg.click(617,212)
+
+#click on results
+pg.click(399,243)
+#click on actions
+pg.click(1872,236)
+#click on export results
+
+time.sleep(0.7)
+
+pg.click(1781,579)
+time.sleep(0.1)
+
+#click on downloads 
+pg.click(117,274)
+time.sleep(0.1)
+
+#click gap 1% down folder 
+pg.moveTo(418,222)
+time.sleep(0.1)
+
+pg.click(418,222)
+pg.click(418,222)
+time.sleep(0.1)
+
+#click save as type
+pg.click(680,500)
+time.sleep(0.1)
+#click csv
+pg.click(518,560)
+time.sleep(0.1)
+
+#click save
+pg.click(747,570)
+
+#if no tickers in list, can't export, so click on ok on the error message, then go to next tab
+
+#click ok on error message
+pg.click(1296,575)
+
+
+
+#ninth tab (4-4.8)
+pg.click(660,212)
+
+#click on results
+pg.click(399,243)
+#click on actions
+pg.click(1872,236)
+#click on export results
+
+time.sleep(0.7)
+
+pg.click(1781,579)
+time.sleep(0.1)
+
+#click on downloads 
+pg.click(117,274)
+time.sleep(0.1)
+
+#click gap 1% down folder 
+pg.moveTo(418,222)
+time.sleep(0.1)
+
+pg.click(418,222)
+pg.click(418,222)
+time.sleep(0.1)
+
+#click save as type
+pg.click(680,500)
+time.sleep(0.1)
+#click csv
+pg.click(518,560)
+time.sleep(0.1)
+
+#click save
+pg.click(747,570)
+
+#if no tickers in list, can't export, so click on ok on the error message, then go to next tab
+
+#click ok on error message
+pg.click(1296,575)
+
+
+
+#tenth tab (4.8-5.6)
+pg.click(703,206)
+
+#click on results
+pg.click(399,243)
+#click on actions
+pg.click(1872,236)
+#click on export results
+
+time.sleep(0.7)
+
+pg.click(1781,579)
+time.sleep(0.1)
+
+#click on downloads 
+pg.click(117,274)
+time.sleep(0.1)
+
+#click gap 1% down folder 
+pg.moveTo(418,222)
+time.sleep(0.1)
+
+pg.click(418,222)
+pg.click(418,222)
+time.sleep(0.1)
+
+#click save as type
+pg.click(680,500)
+time.sleep(0.1)
+#click csv
+pg.click(518,560)
+time.sleep(0.1)
+
+#click save
+pg.click(747,570)
+
+#if no tickers in list, can't export, so click on ok on the error message, then go to next tab
+
+#click ok on error message
+pg.click(1296,575)
+
+
+
+#11th tab (5.6-6.6)
+pg.click(755,206)
+
+#click on results
+pg.click(399,243)
+#click on actions
+pg.click(1872,236)
+#click on export results
+
+time.sleep(0.7)
+
+pg.click(1781,579)
+time.sleep(0.1)
+
+#click on downloads 
+pg.click(117,274)
+time.sleep(0.1)
+
+#click gap 1% down folder 
+pg.moveTo(418,222)
+time.sleep(0.1)
+
+pg.click(418,222)
+pg.click(418,222)
+time.sleep(0.1)
+
+#click save as type
+pg.click(680,500)
+time.sleep(0.1)
+#click csv
+pg.click(518,560)
+time.sleep(0.1)
+
+#click save
+pg.click(747,570)
+
+#if no tickers in list, can't export, so click on ok on the error message, then go to next tab
+
+#click ok on error message
+pg.click(1296,575)
+
+
+
+#12th tab (6.6-7.6)
+pg.click(810,206)
+
+#click on results
+pg.click(399,243)
+#click on actions
+pg.click(1872,236)
+#click on export results
+
+time.sleep(0.7)
+
+pg.click(1781,579)
+time.sleep(0.1)
+
+#click on downloads 
+pg.click(117,274)
+time.sleep(0.1)
+
+#click gap 1% down folder 
+pg.moveTo(418,222)
+time.sleep(0.1)
+
+pg.click(418,222)
+pg.click(418,222)
+time.sleep(0.1)
+
+#click save as type
+pg.click(680,500)
+time.sleep(0.1)
+#click csv
+pg.click(518,560)
+time.sleep(0.1)
+
+#click save
+pg.click(747,570)
+
+#if no tickers in list, can't export, so click on ok on the error message, then go to next tab
+
+#click ok on error message
+pg.click(1296,575)
+
+
+
+#13th tab (7.6-9)
+pg.click(865,206)
+
+#click on results
+pg.click(399,243)
+#click on actions
+pg.click(1872,236)
+#click on export results
+
+time.sleep(0.7)
+
+pg.click(1781,579)
+time.sleep(0.1)
+
+#click on downloads 
+pg.click(117,274)
+time.sleep(0.1)
+
+#click gap 1% down folder 
+pg.moveTo(418,222)
+time.sleep(0.1)
+
+pg.click(418,222)
+pg.click(418,222)
+time.sleep(0.1)
+
+#click save as type
+pg.click(680,500)
+time.sleep(0.1)
+#click csv
+pg.click(518,560)
+time.sleep(0.1)
+
+#click save
+pg.click(747,570)
+
+#if no tickers in list, can't export, so click on ok on the error message, then go to next tab
+
+#click ok on error message
+pg.click(1296,575)
+
+
+
+#14th tab (9-12)
+pg.click(920,206)
+
+#click on results
+pg.click(399,243)
+#click on actions
+pg.click(1872,236)
+#click on export results
+
+time.sleep(0.7)
+
+pg.click(1781,579)
+time.sleep(0.1)
+
+#click on downloads 
+pg.click(117,274)
+time.sleep(0.1)
+
+#click gap 1% down folder 
+pg.moveTo(418,222)
+time.sleep(0.1)
+
+pg.click(418,222)
+pg.click(418,222)
+time.sleep(0.1)
+
+#click save as type
+pg.click(680,500)
+time.sleep(0.1)
+#click csv
+pg.click(518,560)
+time.sleep(0.1)
+
+#click save
+pg.click(747,570)
+
+#if no tickers in list, can't export, so click on ok on the error message, then go to next tab
+
+#click ok on error message
+pg.click(1296,575)
+
+
+
+#15th tab (12-16)
+pg.click(970,206)
+
+#click on results
+pg.click(399,243)
+#click on actions
+pg.click(1872,236)
+#click on export results
+
+time.sleep(0.7)
+
+pg.click(1781,579)
+time.sleep(0.1)
+
+#click on downloads 
+pg.click(117,274)
+time.sleep(0.1)
+
+#click gap 1% down folder 
+pg.moveTo(418,222)
+time.sleep(0.1)
+
+pg.click(418,222)
+pg.click(418,222)
+time.sleep(0.1)
+
+#click save as type
+pg.click(680,500)
+time.sleep(0.1)
+#click csv
+pg.click(518,560)
+time.sleep(0.1)
+
+#click save
+pg.click(747,570)
+
+#if no tickers in list, can't export, so click on ok on the error message, then go to next tab
+
+#click ok on error message
+pg.click(1296,575)
+
+
+
+#16th tab (16-21)
+pg.click(1015,206)
+
+#click on results
+pg.click(399,243)
+#click on actions
+pg.click(1872,236)
+#click on export results
+
+time.sleep(0.7)
+
+pg.click(1781,579)
+time.sleep(0.1)
+
+#click on downloads 
+pg.click(117,274)
+time.sleep(0.1)
+
+#click gap 1% down folder 
+pg.moveTo(418,222)
+time.sleep(0.1)
+
+pg.click(418,222)
+pg.click(418,222)
+time.sleep(0.1)
+
+#click save as type
+pg.click(680,500)
+time.sleep(0.1)
+#click csv
+pg.click(518,560)
+time.sleep(0.1)
+
+#click save
+pg.click(747,570)
+
+#if no tickers in list, can't export, so click on ok on the error message, then go to next tab
+
+#click ok on error message
+pg.click(1296,575)
+
+
+
+#17th tab (21-29)
+pg.click(1070,206)
+
+#click on results
+pg.click(399,243)
+#click on actions
+pg.click(1872,236)
+#click on export results
+
+time.sleep(0.7)
+
+pg.click(1781,579)
+time.sleep(0.1)
+
+#click on downloads 
+pg.click(117,274)
+time.sleep(0.1)
+
+#click gap 1% down folder 
+pg.moveTo(418,222)
+time.sleep(0.1)
+
+pg.click(418,222)
+pg.click(418,222)
+time.sleep(0.1)
+
+#click save as type
+pg.click(680,500)
+time.sleep(0.1)
+#click csv
+pg.click(518,560)
+time.sleep(0.1)
+
+#click save
+pg.click(747,570)
+
+#if no tickers in list, can't export, so click on ok on the error message, then go to next tab
+
+#click ok on error message
+pg.click(1296,575)
+
+
+
+#18th tab (29-45)
+pg.click(1125,206)
+
+#click on results
+pg.click(399,243)
+#click on actions
+pg.click(1872,236)
+#click on export results
+
+time.sleep(0.7)
+
+pg.click(1781,579)
+time.sleep(0.1)
+
+#click on downloads 
+pg.click(117,274)
+time.sleep(0.1)
+
+#click gap 1% down folder 
+pg.moveTo(418,222)
+time.sleep(0.1)
+
+pg.click(418,222)
+pg.click(418,222)
+time.sleep(0.1)
+
+#click save as type
+pg.click(680,500)
+time.sleep(0.1)
+#click csv
+pg.click(518,560)
+time.sleep(0.1)
+
+#click save
+pg.click(747,570)
+
+#if no tickers in list, can't export, so click on ok on the error message, then go to next tab
+
+#click ok on error message
+pg.click(1296,575)
+
+
+
+#19th tab (45-95)
+pg.click(1185,206)
+
+#click on results
+pg.click(399,243)
+#click on actions
+pg.click(1872,236)
+#click on export results
+
+time.sleep(0.7)
+
+pg.click(1781,579)
+time.sleep(0.1)
+
+#click on downloads 
+pg.click(117,274)
+time.sleep(0.1)
+
+#click gap 1% down folder 
+pg.moveTo(418,222)
+time.sleep(0.1)
+
+pg.click(418,222)
+pg.click(418,222)
+time.sleep(0.1)
+
+#click save as type
+pg.click(680,500)
+time.sleep(0.1)
+#click csv
+pg.click(518,560)
+time.sleep(0.1)
+
+#click save
+pg.click(747,570)
+
+#if no tickers in list, can't export, so click on ok on the error message, then go to next tab
+
+#click ok on error message
+pg.click(1296,575)
+
+#20th tab (95+)
+pg.click(1230,206)
+
+#click on results
+pg.click(399,243)
+#click on actions
+pg.click(1872,236)
+#click on export results
+
+time.sleep(0.7)
+
+pg.click(1781,579)
+time.sleep(0.1)
+
+#click on downloads 
+pg.click(117,274)
+time.sleep(0.1)
+
+#click gap 1% down folder 
+pg.moveTo(418,222)
+time.sleep(0.1)
+
+pg.click(418,222)
+pg.click(418,222)
+time.sleep(0.1)
+
+#click save as type
+pg.click(680,500)
+time.sleep(0.1)
+#click csv
+pg.click(518,560)
+time.sleep(0.1)
+
+#click save
+pg.click(747,570)
+
+#if no tickers in list, can't export, so click on ok on the error message, then go to next tab
+
+#click ok on error message
+pg.click(1296,575)
+
+
+# In[ ]:
+
+
+
+
